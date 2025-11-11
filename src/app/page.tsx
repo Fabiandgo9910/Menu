4a544359e3b4f2ee9882e48875eb5d799960639f
Fabiandgo <fdgo.9910@gmail.com>
2025-11-11 17:05:04 +0100

    meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,40 @@ import Newsletter from '@/app/components/Home/Newsletter'
 import { Metadata } from 'next'
 import ContactForm from './components/Contact/Form'
 export const metadata: Metadata = {
-  title: `Jenkin's`,
+  title: "Jenkin's - Restaurante y Café",
+  description: "Disfruta de los mejores platos, bebidas y postres en Jenkin's. Menú para compartir, ensaladas frescas, hamburguesas, carnes y postres irresistibles.",
+  keywords: [
+    "restaurante", "café", "comida", "hamburguesas", "ensaladas",
+    "postres", "bebidas", "menú", "Jenkin's", "parrilla", "steak"
+  ],
+  authors: [{ name: "Jenkin's", url: "https://tusitio.com" }],
+  category: "Food & Drink",
   icons: {
-  icon: '/favicon.ico',
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png', // si quieres icono para iOS
+  },
+  openGraph: {
+    title: "Jenkin's - Restaurante y Café",
+    description: "Disfruta de los mejores platos, bebidas y postres en Jenkin's.",
+    url: "https://tusitio.com",
+    siteName: "Jenkin's",
+    images: [
+      {
+        url: "/images/og-image.webp",
+        width: 1200,
+        height: 630,
+        alt: "Jenkin's Restaurante",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jenkin's - Restaurante y Café",
+    description: "Disfruta de los mejores platos, bebidas y postres en Jenkin's.",
+    images: ["/images/og-image.webp"],
+    site: "@tuTwitter",
+    creator: "@tuTwitter",
   },
 }
 
